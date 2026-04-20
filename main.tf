6339611e797764b72ae41ce5dd85aac4b052ea0a
kyrolles <kyrolles1raafat2@gmail.com>
2026-04-21 00:53:36 +0200

    finshed task lec 33

--- main.tf
+++ main.tf
@@ -15,8 +15,10 @@ provider "aws" {
 
 # Create a VPC
 resource "aws_vpc" "example" {
-  cidr_block = "10.0.0.0/16"
+  cidr_block           = "10.0.0.0/16"
+  enable_dns_hostnames = true
 }
+
 
 resource "aws_subnet" "subnet1" {
   vpc_id            = aws_vpc.example.id
@@ -27,4 +29,103 @@ resource "aws_subnet" "subnet2" {
   vpc_id            = aws_vpc.example.id
   cidr_block        = "10.0.2.0/24"
   availability_zone = "us-east-1b"
+}
+
+resource "aws_internet_gateway" "gw" {
+  vpc_id = aws_vpc.example.id
+  tags = {
+    Name = "internet-gateway-public"
+  }
+}
+
+resource "aws_route_table" "example" {
+  vpc_id = aws_vpc.example.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.gw.id
+  }
+
+  # route {
+  #   ipv6_cidr_block        = "::/0"
+  #   egress_only_gateway_id = aws_egress_only_internet_gateway.example.id
+  # }
+
+  tags = {
+    Name = "example_1"
+  }
+}
+
+resource "aws_route_table_association" "example1" {
+  subnet_id      = aws_subnet.subnet1.id
+  route_table_id = aws_route_table.example.id
+}
+
+resource "aws_route_table_association" "example2" {
+  subnet_id      = aws_subnet.subnet2.id
+  route_table_id = aws_route_table.example.id
+}
+
+resource "aws_s3_bucket" "example" {
+  bucket_prefix = "abc-k-"
+
+}
+
+resource "aws_security_group" "allow_tls" {
+  name        = "sg_1"
+  description = "Allow 80,ssh inbound from every where, Allow all outbound traffic"
+  vpc_id      = aws_vpc.example.id
+
+  tags = {
+    Name = "allow_tls"
+  }
+}
+
+resource "aws_vpc_security_group_ingress_rule" "allow_tls_ipv4" {
+  security_group_id = aws_security_group.allow_tls.id
+  cidr_ipv4         = "0.0.0.0/0"
+  from_port         = 80
+  ip_protocol       = "tcp"
+  to_port           = 80
+}
+
+resource "aws_vpc_security_group_ingress_rule" "allow_tls_ipv6" {
+  security_group_id = aws_security_group.allow_tls.id
+  cidr_ipv4         = "0.0.0.0/0"
+  from_port         = 22
+  ip_protocol       = "tcp"
+  to_port           = 22
+}
+
+resource "aws_vpc_security_group_egress_rule" "allow_all_traffic_ipv4" {
+  security_group_id = aws_security_group.allow_tls.id
+  cidr_ipv4         = "0.0.0.0/0"
+  ip_protocol       = "-1" # semantically equivalent to all ports
+}
+
+resource "aws_key_pair" "deployer" {
+  key_name   = "kok-key"
+  public_key = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIJOfiLpScMk/yv1VDc1JAXraFF4+RmQdeX19BSHu7WZN kyrl@kok"
+}
+
+resource "aws_instance" "example" {
+  ami                         = "ami-098e39bafa7e7303d"
+  instance_type               = "t3.micro"
+  subnet_id                   = aws_subnet.subnet1.id
+  key_name                    = aws_key_pair.deployer.key_name
+  vpc_security_group_ids      = [aws_security_group.allow_tls.id]
+  associate_public_ip_address = true
+  user_data                   = <<-EOF
+                #!/bin/bash
+                sudo dnf install nginx -y
+                sudo systemctl start nginx
+                EOF
+
+  tags = {
+    Name = "HelloWorld"
+  }
+}
+
+output "ec2_public_dns" {
+  value = aws_instance.example.public_dns
 }
